e07d1d62624e2d93aa943b82025eed020eeed7a2
Michijiz <miki.candela@hotmail.it>
2024-11-29 18:14:10 +0100

    modifica file routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// app-routing.module.ts
 import { Routes } from '@angular/router';
+import { PrimaPaginaComponent } from './prima-pagina/prima-pagina.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PrimaPaginaComponent },
+  { path: '**', redirectTo: '' },
+];
